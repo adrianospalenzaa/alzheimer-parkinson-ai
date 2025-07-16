7eea845d0f0237e1052db3bf7243e0cc086b1b7b
Adriano Spalenza <108499428+adrianospalenzaa@users.noreply.github.com>
2025-07-16 07:55:40 -0300

    tratamento de dados

--- preprocessing/voice_analysis/scripts/extrair_features.py
+++ preprocessing/voice_analysis/scripts/extrair_features.py
@@ -50,6 +50,6 @@ def extrair_features(audio_path, pasta_saida='"C:/Users/adria/PycharmProjects/Wo
 
 # Teste unitário
 if __name__ == "__main__":
-    caminho = "C:/Users/adria/PycharmProjects/WorkSpace/alzheimer-parkinson-ai/data/audio/raw_wav/voice002.wav"
+    caminho = "C:/Users/adria/PycharmProjects/WorkSpace/alzheimer-parkinson-ai/data/audio/raw_wav/voice099.wav"
     extrair_features(caminho, pasta_saida="C:/Users/adria/PycharmProjects/WorkSpace/alzheimer-parkinson-ai/data/audio/features")
 
